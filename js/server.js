eebc5492a7e5256ecc04568cd06f054ab0162e13
Jamie Ferguson <jamie.d.f@gmail.com>
2014-02-27 21:12:08 +0000

    - Added some error checking

--- js/server.js
+++ js/server.js
@@ -36,17 +36,22 @@ app.get('/api/images', function(req, res) {
 
     var accessToken = req.query.code;
 
+    if(!accessToken) {
+        return res.send(500, "No code/accessToken in request");
+    }
+
     res.set("Content-Type", "application/json");
-    //FB.setAccessToken('CAACEdEose0cBADs0iHmjWGVcNBsZCmG1EFhZBYQ3AZBYKm0pDKUhyzAbHjaUu3ug5vGlXmNaMc3WEyyAArZCfhTLR2RAiJZCUfS6FlIdKbZB6Ysen5EZBXHTVOdcnrEKDPMEDzoAhl2oruNzZAkxP2MubwAjnqJ7cMIx5pvrxfSs9jcNE4dAC6IKsigaYvd8NDQZD');
+    //FB.setAccessToken('CAACEdEose0cBAA2g1RwGq004OFraoxZCXwy7XEskZAgKkQZBVmFkJml7R6hnZB0rKcutW6vplDfPbdRzM3fS8PZC4oEdujc8V6io6xYOECCZC1yLYWcdQd2OBXtgZBVVbJxkaZAhKZCGwD2GR5uPwxvZAlC7CnUmBctZAZBS46KSq6ewNKFpD6hj6fjmj07EkLjGAaL9uC7aQYPFdAZDZD');
     FB.setAccessToken(accessToken);
 
 
     FB.api('fql', { q: 'select uid from user where uid = me()' }, function (fbRes) {
       if(!fbRes || fbRes.error) {
         console.log(!fbRes ? 'error occurred' : res.error);
-        return;
+        return res.send(500, "Failed to get uid");
       }
 
+        console.log(JSON.stringify(fbRes.data[0].uid));
         fetchUserPhotos(fbRes.data[0].uid, res);
 
     });
@@ -54,36 +59,41 @@ app.get('/api/images', function(req, res) {
 
 function fetchUserPhotos(uid, res) {
 
-    FB.api(uid +'', { fields : ['id','name', 'photos.limit(1000)']}, function (fbRes) {
+    FB.api(uid +'', { fields : ['id','name', 'photos.since(1393534770).limit(1000)']}, function (fbRes) {
         if(!fbRes || fbRes.error) {
             console.log(!fbRes ? 'error occurred' : fbRes.error);
-            return;
+            return res.send(500, "Failed to request photos");
         }
 
         var mapData = {
-             type : "FeatureCollection"
+            type : "FeatureCollection",
+            features: []
         };
 
-        mapData.features = _.filter(_.map(fbRes.photos.data, function(photo) {
 
 
-            if(photo.place && photo.place.location && photo.place.location.longitude) {
-                return  {
-                    type: "Feature",
-                    properties : {
-                        title : photo.name,
-                        image : _.first(photo.images)
-                    },
-                    geometry : {
-                        type : "Point",
-                        coordinates : [
-                            photo.place.location.longitude,
-                            photo.place.location.latitude
-                        ]
-                    }
-                };
-            }
-        }));
+        if(fbRes.photos) {
+            mapData.features = _.filter(_.map(fbRes.photos.data, function(photo) {
+
+
+                if(photo.place && photo.place.location && photo.place.location.longitude) {
+                    return  {
+                        type: "Feature",
+                        properties : {
+                            title : photo.name,
+                            image : _.first(photo.images)
+                        },
+                        geometry : {
+                            type : "Point",
+                            coordinates : [
+                                photo.place.location.longitude,
+                                photo.place.location.latitude
+                            ]
+                        }
+                    };
+                }
+            }));
+        }
 
         res.send(mapData);
     });
