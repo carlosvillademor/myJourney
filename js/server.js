394e9ca57000faff116a6477063e723acecb2665
Luis Silva <luis.dasilva@mailonline.co.uk>
2014-02-27 16:22:15 +0000

    [Luis] add static folder

--- js/server.js
+++ js/server.js
@@ -1,9 +1,18 @@
 var express = require('express'),
     app = express(),
     mu = require('mu2'),
-    util = require('util');
+    util = require('util'),
+    path = require( 'path' );
 
 mu.root = __dirname;
+
+app.configure(function () {
+    var staticPath = path.resolve ( __dirname + '/../static' );
+    console.log(staticPath);
+    app.use( express.static( staticPath ) );
+
+    app.use( express.logger() );
+});
 
 app.get('/', function(req, res) {
     console.log('get request to /');
@@ -11,6 +20,8 @@ app.get('/', function(req, res) {
     console.log('homePage is', homePage);
     util.pump(homePage, res);
 });
+
+app.use(express.static(__dirname + '/static'));
 
 app.listen(process.env.PORT || 5000);
 
