38c06b69a566d15a2a5798601462a354821c6526
Luis Silva <luis.dasilva@mailonline.co.uk>
2014-02-27 20:08:33 +0000

    Luis: add create page and jquery and plugin

--- src/js/map.js
+++ src/js/map.js
@@ -2,21 +2,23 @@
     'use strict';
 
     module.exports = function () {
-        var map = L.mapbox.map('map', 'fetz.hcpe8ip9')
-            .setView([38, -102.0], 9);
 
-        var featureLayer = L.mapbox.featureLayer()
-            .loadURL('temp/map.json')
-            .addTo(map);
+        if ($('#map').length > 0) {
+            var map = L.mapbox.map('map', 'fetz.hcpe8ip9')
+                .setView([38, -102.0], 9);
 
-        featureLayer.on('ready', function() {
-            map.fitBounds(featureLayer.getBounds());
-        });
+            var featureLayer = L.mapbox.featureLayer()
+                .loadURL('temp/map.json')
+                .addTo(map);
 
-        featureLayer.on('click', function(e) {
-            map.panTo(e.layer.getLatLng());
-        });
+            featureLayer.on('ready', function() {
+                map.fitBounds(featureLayer.getBounds());
+            });
 
+            featureLayer.on('click', function(e) {
+                map.panTo(e.layer.getLatLng());
+            });
+        }
 
         return map;
     };
